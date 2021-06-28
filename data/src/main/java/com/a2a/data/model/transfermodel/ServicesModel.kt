acfe87c5b4b1681523f7eed51deb16b5a22d0804
firassalaman <fsalman@access2arabia.com>
2021-06-28 13:38:58 +0300

    fix   ServicesModel.kt

---
+++ data/src/main/java/com/a2a/data/model/transfermodel/ServicesModel.kt
@@ -0,0 +1,3 @@
+package com.a2a.data.model.transfermodel
+
+class ServicesModel (val serviceImage: Int, val serviceType:String)
--- data/src/main/java/com/a2a/data/model/transfermodel/TransferModel.kt
+++
@@ -1,3 +0,0 @@
-package com.a2a.data.model.transfermodel
-
-class TransferModel (val serviceImage: Int, val serviceType:String)
