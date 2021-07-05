0a248da2c2e69ee72186d6b31927e668e679a8f6
firassalaman <fsalman@access2arabia.com>
2021-07-05 14:41:09 +0300

    Build  LookUp

---
+++ data/src/main/java/com/a2a/data/model/lookup/LookUpResponse.kt
@@ -0,0 +1,180 @@
+package com.a2a.data.model.lookup
+import com.google.gson.annotations.SerializedName
+
+
+data class LookUpResponse(
+    @SerializedName("A2ARequest")
+    var a2ARequest: A2ARequest = A2ARequest(),
+    @SerializedName("A2AResponse")
+    var a2AResponse: A2AResponse = A2AResponse(),
+    @SerializedName("ErrorMsg")
+    var errorMsg: ErrorMsg = ErrorMsg()
+) {
+    data class A2ARequest(
+        @SerializedName("Body")
+        var body: Body = Body(),
+        @SerializedName("Footer")
+        var footer: Footer = Footer(),
+        @SerializedName("Header")
+        var header: Header = Header()
+    ) {
+        data class Body(
+            @SerializedName("LocX")
+            var locX: String = "",
+            @SerializedName("LocY")
+            var locY: String = "",
+            @SerializedName("LookUpName")
+            var lookUpName: String = ""
+        )
+
+        data class Footer(
+            @SerializedName("Signature")
+            var signature: String = ""
+        )
+
+        data class Header(
+            @SerializedName("BankCode")
+            var bankCode: String = "",
+            @SerializedName("Channel")
+            var channel: String = "",
+            @SerializedName("ConnectorID")
+            var connectorID: Any = Any(),
+            @SerializedName("Device")
+            var device: Any = Any(),
+            @SerializedName("DeviceID")
+            var deviceID: String = "",
+            @SerializedName("DeviceToken")
+            var deviceToken: Any = Any(),
+            @SerializedName("GuidID")
+            var guidID: String = "",
+            @SerializedName("MethodName")
+            var methodName: String = "",
+            @SerializedName("Password")
+            var password: String = "",
+            @SerializedName("RegionCode")
+            var regionCode: String = "",
+            @SerializedName("ServiceID")
+            var serviceID: Int = 0,
+            @SerializedName("SessionID")
+            var sessionID: Any = Any(),
+            @SerializedName("SrvID")
+            var srvID: String = "",
+            @SerializedName("TimeStamp")
+            var timeStamp: String = "",
+            @SerializedName("UserID")
+            var userID: String = ""
+        )
+    }
+
+    data class A2AResponse(
+        @SerializedName("Body")
+        var body: Body = Body(),
+        @SerializedName("Footer")
+        var footer: Footer = Footer(),
+        @SerializedName("Header")
+        var header: Header = Header()
+    ) {
+        data class Body(
+            @SerializedName("Banks")
+            var banks: List<Bank> = listOf(),
+            @SerializedName("Currency")
+            var currency: List<Currency> = listOf(),
+            @SerializedName("RTGSAccTypes")
+            var rTGSAccTypes: List<RTGSAccType> = listOf(),
+            @SerializedName("RTGSTransferList")
+            var rTGSTransferList: List<RTGSTransfer> = listOf()
+        ) {
+            data class Bank(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("AValue")
+                var aValue: String = "",
+                @SerializedName("DescEnAr")
+                var descEnAr: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("EValue")
+                var eValue: String = ""
+            )
+
+            data class Currency(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("Code")
+                var code: String = "",
+                @SerializedName("DescEnAr")
+                var descEnAr: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("ID")
+                var iD: Int = 0,
+                @SerializedName("ISOCode")
+                var iSOCode: String = ""
+            )
+
+            data class RTGSAccType(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("AValue")
+                var aValue: String = "",
+                @SerializedName("DescEnAr")
+                var descEnAr: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("EValue")
+                var eValue: String = ""
+            )
+
+            data class RTGSTransfer(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("AValue")
+                var aValue: String = "",
+                @SerializedName("DescEnAr")
+                var descEnAr: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("EValue")
+                var eValue: String = ""
+            )
+        }
+
+        data class Footer(
+            @SerializedName("Signature")
+            var signature: String = ""
+        )
+
+        data class Header(
+            @SerializedName("GuidID")
+            var guidID: String = "",
+            @SerializedName("RegionCode")
+            var regionCode: String = "",
+            @SerializedName("Result")
+            var result: Result = Result(),
+            @SerializedName("SessionID")
+            var sessionID: Any = Any(),
+            @SerializedName("SrvID")
+            var srvID: Int = 0,
+            @SerializedName("TimeStamp")
+            var timeStamp: String = ""
+        ) {
+            data class Result(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("ErrorCode")
+                var errorCode: Int = 0
+            )
+        }
+    }
+
+    data class ErrorMsg(
+        @SerializedName("ADesc")
+        var aDesc: String = "",
+        @SerializedName("EDesc")
+        var eDesc: String = "",
+        @SerializedName("ErrorCode")
+        var errorCode: Int = 0
+    )
+}
---
+++ data/src/main/java/com/a2a/data/repository/lookup/LookUpRepository.kt
@@ -0,0 +1,56 @@
+package com.a2a.data.repository.lookup
+
+import com.a2a.data.constants.Constants
+import com.a2a.data.datasource.RemoteDataSource
+import com.a2a.data.extentions.formatToViewTimeStamp
+import com.a2a.data.model.lookup.LookUpPostData
+
+
+import com.a2a.data.repository.BaseRepository
+import com.a2a.network.Resource
+import java.util.*
+import javax.inject.Inject
+
+class LookUpRepository @Inject constructor(
+    private val remoteDataSource: RemoteDataSource
+) : BaseRepository(){
+
+
+    suspend fun <T> getLookUp(
+        LookUpName: String
+    ): Resource<T>? {
+        val postData = LookUpPostData()
+        postData.apply {
+            a2ARequest?.apply {
+                header?.apply {
+                    bankCode = Constants.BankCode
+                    regionCode = Constants.RegionCode
+                    srvID = "IntFund"
+                    serviceID = 0
+                    methodName = ""
+                    userID = Constants.UserID
+                    password = Constants.Password
+                    channel = Constants.Channel
+                    timeStamp = Date().formatToViewTimeStamp()
+
+                    deviceID = Constants.DeviceID
+                }
+
+                a2ARequest?.body?.apply {
+                    lookUpName= "RTGSTransferList,RTGSAccTypes,Currency,Banks"
+                    locX ="31.9500"
+                    locY= "35.9333"
+                }
+                a2ARequest?.footer?.apply {
+                    signature = ""
+                }
+            }
+        }
+        return safeApiCall(postData) {
+            remoteDataSource.baseRequest(postData)
+
+        }
+
+    }
+
+}
