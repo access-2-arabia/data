f01ed961e9fdb0be1e40af22a3289dd3c15938be
firassalaman <fsalman@access2arabia.com>
2021-07-07 00:11:22 +0300

    Build  Model  For  Base Request 20

--- data/src/main/java/com/a2a/data/model/accountlist/AccountListPostData.kt
+++ data/src/main/java/com/a2a/data/model/accountlist/AccountListPostData.kt
@@ -1,138 +1,20 @@
 package com.a2a.data.model.accountlist
 
+import com.a2a.data.model.CustProfile
 import com.google.gson.annotations.SerializedName
 
 
 data class AccountListPostData(
-    @SerializedName("a2ARequest")
-    var a2ARequest: A2ARequest = A2ARequest()
+    @SerializedName("Body")
+    var body: Body = Body(),
 ) {
-    data class A2ARequest(
-        @SerializedName("Body")
-        var body: Body = Body(),
-        @SerializedName("Footer")
-        var footer: Footer = Footer(),
-        @SerializedName("Header")
-        var header: Header = Header()
-    ) {
-        data class Body(
-            @SerializedName("BankRef")
-            var bankRef: String = "",
-            @SerializedName("BranchCode")
-            var branchCode: String = "",
-            @SerializedName("CustProfile")
-            var custProfile: CustProfile = CustProfile()
-        ) {
-            data class CustProfile(
-                @SerializedName("AName")
-                var aName: String = "",
-                @SerializedName("ANameShort")
-                var aNameShort: String = "",
-                @SerializedName("Address1")
-                var address1: String = "",
-                @SerializedName("Address2")
-                var address2: String = "",
-                @SerializedName("AddressCity")
-                var addressCity: String = "",
-                @SerializedName("AddressCountry")
-                var addressCountry: String = "",
-                @SerializedName("BirthDate")
-                var birthDate: String = "",
-                @SerializedName("Branch")
-                var branch: String = "",
-                @SerializedName("BuildingNo")
-                var buildingNo: String = "",
-                @SerializedName("CID")
-                var cID: Int = 0,
-                @SerializedName("CustID")
-                var custID: String = "",
-                @SerializedName("CustImage")
-                var custImage: String = "",
-                @SerializedName("CustMnemonic")
-                var custMnemonic: String = "",
-                @SerializedName("CustType")
-                var custType: Int = 0,
-                @SerializedName("detPrivateNationality")
-                var detPrivateNationality: String = "",
-                @SerializedName("DocNo")
-                var docNo: String = "",
-                @SerializedName("DocNo1")
-                var docNo1: String = "",
-                @SerializedName("DocValidDate")
-                var docValidDate: String = "",
-                @SerializedName("EMail")
-                var eMail: String = "",
-                @SerializedName("EName")
-                var eName: String = "",
-                @SerializedName("ENameShort")
-                var eNameShort: String = "",
-                @SerializedName("Gender")
-                var gender: String = "",
-                @SerializedName("ISOCode")
-                var iSOCode: String = "",
-                @SerializedName("ISOCode2")
-                var iSOCode2: String = "",
-                @SerializedName("LWPT")
-                var lWPT: Boolean = false,
-                @SerializedName("LWTD")
-                var lWTD: Boolean = false,
-                @SerializedName("Lang")
-                var lang: String = "",
-                @SerializedName("MobileNumber")
-                var mobileNumber: String = "",
-                @SerializedName("MobileNumberMasked")
-                var mobileNumberMasked: String = "",
-                @SerializedName("Nationality")
-                var nationality: String = "",
-                @SerializedName("NationalityID")
-                var nationalityID: String = "",
-                @SerializedName("placeOfBirth")
-                var placeOfBirth: String = "",
-                @SerializedName("RID")
-                var rID: Int = 0,
-                @SerializedName("RepID")
-                var repID: Int = 0,
-                @SerializedName("TermsAndCondition")
-                var termsAndCondition: Int = 0
+    data class Body(
+        @SerializedName("BankRef")
+        var bankRef: String = "",
+        @SerializedName("BranchCode")
+        var branchCode: String = "",
+        @SerializedName("CustProfile")
+        var custProfile: CustProfile = CustProfile()
+    )
 
-
-            )
-        }
-
-        data class Footer(
-            @SerializedName("Signature")
-            var signature: String = ""
-        )
-
-        data class Header(
-            @SerializedName("BankCode")
-            var bankCode: String = "",
-            @SerializedName("Channel")
-            var channel: String = "",
-            @SerializedName("DeviceID")
-            var deviceID: String = "",
-            @SerializedName("GuidID")
-            var guidID: String = "",
-            @SerializedName("MethodName")
-            var methodName: String = "",
-            @SerializedName("Password")
-            var password: String = "",
-            @SerializedName("RegionCode")
-            var regionCode: String = "",
-            @SerializedName("ServiceID")
-            var serviceID: Int = 0,
-            @SerializedName("SrvID")
-            var srvID: String = "",
-            @SerializedName("TimeStamp")
-            var timeStamp: String = "",
-            @SerializedName("UserID")
-            var userID: String = "",
-            @SerializedName("DeviceToken")
-            var deviceToken: String = "",
-            @SerializedName("Device")
-            var device: String = "",
-
-
-            )
-    }
 }
--- data/src/main/java/com/a2a/data/repository/accounts/AccountsRepository.kt
+++ data/src/main/java/com/a2a/data/repository/accounts/AccountsRepository.kt
@@ -3,11 +3,11 @@ package com.a2a.data.repository.accounts
 import MemoryCacheImpl
 import com.a2a.data.constants.Constants
 import com.a2a.data.datasource.RemoteDataSource
-import com.a2a.data.extentions.formatToViewTimeStamp
 import com.a2a.data.model.accountlist.AccountListPostData
+import com.a2a.data.model.common.A2ARequest
+import com.a2a.data.model.common.BaseRequestModel
 import com.a2a.data.repository.BaseRepository
 import com.a2a.network.Resource
-import java.util.*
 import javax.inject.Inject
 
 class AccountsRepository @Inject constructor(
@@ -15,70 +15,82 @@ class AccountsRepository @Inject constructor(
 ) : BaseRepository() {
 
     suspend fun <T> getAccountsList(): Resource<T>? {
-        val postData = AccountListPostData()
-        postData.apply {
-            a2ARequest?.apply {
-                header?.apply {
-                    bankCode = Constants.BankCode
-                    regionCode = Constants.RegionCode
-                    srvID = "DashBoard"
-                    serviceID = 0
-                    methodName = ""
-                    userID = Constants.UserID
-                    password = Constants.Password
-                    channel = Constants.Channel
-                    timeStamp = Date().formatToViewTimeStamp()
-                    guidID = Constants.GuidID
-                    deviceID = MemoryCacheImpl.getDeviceId()
-                    deviceToken = Constants.TokenId
-                    device = "Android"
-                }
-
-                a2ARequest?.body?.apply {
-                    custProfile.repID = MemoryCacheImpl.getCustProfile()!!.repID
-                    custProfile.custMnemonic = MemoryCacheImpl.getCustProfile()!!.custMnemonic
-                    custProfile.address1 = MemoryCacheImpl.getCustProfile()!!.address1
-                    custProfile.address2 = MemoryCacheImpl.getCustProfile()!!.address1
-                    custProfile.addressCity = MemoryCacheImpl.getCustProfile()!!.addressCity
-                    custProfile.addressCountry = MemoryCacheImpl.getCustProfile()!!.addressCountry
-                    custProfile.aName = MemoryCacheImpl.getCustProfile()!!.aName
-                    custProfile.aNameShort = MemoryCacheImpl.getCustProfile()!!.aNameShort
-                    custProfile.birthDate = MemoryCacheImpl.getCustProfile()!!.birthDate
-                    custProfile.branch = MemoryCacheImpl.getCustProfile()!!.branch
-                    branchCode = MemoryCacheImpl.getCustProfile()!!.branch
-                    custProfile.cID = MemoryCacheImpl.getCustProfile()!!.cID
-                    custProfile.custID = MemoryCacheImpl.getCustProfile()!!.custID
-                    custProfile.custImage = MemoryCacheImpl.getCustProfile()!!.custImage
-                    custProfile.custMnemonic = MemoryCacheImpl.getCustProfile()!!.custMnemonic
-                    custProfile.custType = MemoryCacheImpl.getCustProfile()!!.custType
-                    custProfile.docNo = MemoryCacheImpl.getCustProfile()!!.docNo
-                    custProfile.docNo1 = MemoryCacheImpl.getCustProfile()!!.docNo1
-                    custProfile.docValidDate = MemoryCacheImpl.getCustProfile()!!.docValidDate
-                    custProfile.eMail = MemoryCacheImpl.getCustProfile()!!.eMail
-                    custProfile.eName = MemoryCacheImpl.getCustProfile()!!.eName
-                    custProfile.eNameShort = MemoryCacheImpl.getCustProfile()!!.eNameShort
-                    custProfile.gender = MemoryCacheImpl.getCustProfile()!!.gender
-                    custProfile.iSOCode = MemoryCacheImpl.getCustProfile()!!.iSOCode
-                    custProfile.iSOCode2 = MemoryCacheImpl.getCustProfile()!!.iSOCode2
-                    custProfile.lang = MemoryCacheImpl.getCustProfile()!!.lang
-                    custProfile.lWPT = MemoryCacheImpl.getCustProfile()!!.lWPT
-                    custProfile.lWTD = MemoryCacheImpl.getCustProfile()!!.lWTD
-                    custProfile.mobileNumber = MemoryCacheImpl.getCustProfile()!!.mobileNumber
-                    custProfile.mobileNumberMasked =
-                        MemoryCacheImpl.getCustProfile()!!.mobileNumberMasked
-                    custProfile.nationality = MemoryCacheImpl.getCustProfile()!!.nationality
-                    custProfile.nationalityID = MemoryCacheImpl.getCustProfile()!!.nationalityID
-                    custProfile.placeOfBirth = MemoryCacheImpl.getCustProfile()!!.placeOfBirth
-                    custProfile.termsAndCondition =
-                        MemoryCacheImpl.getCustProfile()!!.termsAndCondition
-                    branchCode = MemoryCacheImpl.getCustProfile()!!.branch
-
-                }
-                a2ARequest?.footer?.apply {
-                    signature = ""
-                }
-            }
+        val accountPostData = AccountListPostData()
+        accountPostData.apply {
+            body.custProfile = MemoryCacheImpl.getCustProfile()!!
+            body.branchCode=Constants.BankCode
         }
+        val postData =
+            BaseRequestModel(A2ARequest(accountPostData.body, srvID = "DashBoard", serviceIDValue = 0))
+
+//        val postData = AccountListPostData()
+//        postData.apply {
+//            a2ARequest?.apply {
+//                header?.apply {
+//                    bankCode = Constants.BankCode
+//                    regionCode = Constants.RegionCode
+//                    srvID = "DashBoard"
+//                    serviceID = 0
+//                    methodName = ""
+//                    userID = Constants.UserID
+//                    password = Constants.Password
+//                    channel = Constants.Channel
+//                    timeStamp = Date().formatToViewTimeStamp()
+//                    guidID = Constants.GuidID
+//                    deviceID = MemoryCacheImpl.getDeviceId()
+//                    deviceToken = Constants.TokenId
+//                    device = "Android"
+//                }
+//
+//                a2ARequest?.body?.apply {
+//                    custProfile.repID = MemoryCacheImpl.getCustProfile()!!.repID
+//                    custProfile.custMnemonic = MemoryCacheImpl.getCustProfile()!!.custMnemonic
+//                    custProfile.address1 = MemoryCacheImpl.getCustProfile()!!.address1
+//                    custProfile.address2 = MemoryCacheImpl.getCustProfile()!!.address1
+//                    custProfile.addressCity = MemoryCacheImpl.getCustProfile()!!.addressCity
+//                    custProfile.addressCountry = MemoryCacheImpl.getCustProfile()!!.addressCountry
+//                    custProfile.aName = MemoryCacheImpl.getCustProfile()!!.aName
+//                    custProfile.aNameShort = MemoryCacheImpl.getCustProfile()!!.aNameShort
+//                    custProfile.birthDate = MemoryCacheImpl.getCustProfile()!!.birthDate
+//                    custProfile.branch = MemoryCacheImpl.getCustProfile()!!.branch
+//                    branchCode = MemoryCacheImpl.getCustProfile()!!.branch
+//                    custProfile.cID = MemoryCacheImpl.getCustProfile()!!.cID
+//                    custProfile.custID = MemoryCacheImpl.getCustProfile()!!.custID
+//                    custProfile.custImage = MemoryCacheImpl.getCustProfile()!!.custImage
+//                    custProfile.custMnemonic = MemoryCacheImpl.getCustProfile()!!.custMnemonic
+//                    custProfile.custType = MemoryCacheImpl.getCustProfile()!!.custType
+//                    custProfile.docNo = MemoryCacheImpl.getCustProfile()!!.docNo
+//                    custProfile.docNo1 = MemoryCacheImpl.getCustProfile()!!.docNo1
+//                    custProfile.docValidDate = MemoryCacheImpl.getCustProfile()!!.docValidDate
+//                    custProfile.eMail = MemoryCacheImpl.getCustProfile()!!.eMail
+//                    custProfile.eName = MemoryCacheImpl.getCustProfile()!!.eName
+//                    custProfile.eNameShort = MemoryCacheImpl.getCustProfile()!!.eNameShort
+//                    custProfile.gender = MemoryCacheImpl.getCustProfile()!!.gender
+//                    custProfile.iSOCode = MemoryCacheImpl.getCustProfile()!!.iSOCode
+//                    custProfile.iSOCode2 = MemoryCacheImpl.getCustProfile()!!.iSOCode2
+//                    custProfile.lang = MemoryCacheImpl.getCustProfile()!!.lang
+//                    custProfile.lWPT = MemoryCacheImpl.getCustProfile()!!.lWPT
+//                    custProfile.lWTD = MemoryCacheImpl.getCustProfile()!!.lWTD
+//                    custProfile.mobileNumber = MemoryCacheImpl.getCustProfile()!!.mobileNumber
+//                    custProfile.mobileNumberMasked =
+//                        MemoryCacheImpl.getCustProfile()!!.mobileNumberMasked
+//                    custProfile.nationality = MemoryCacheImpl.getCustProfile()!!.nationality
+//                    custProfile.nationalityID = MemoryCacheImpl.getCustProfile()!!.nationalityID
+//                    custProfile.placeOfBirth = MemoryCacheImpl.getCustProfile()!!.placeOfBirth
+//                    custProfile.termsAndCondition =
+//                        MemoryCacheImpl.getCustProfile()!!.termsAndCondition
+//                    branchCode = MemoryCacheImpl.getCustProfile()!!.branch
+//
+//                }
+//                a2ARequest?.footer?.apply {
+//                    signature = ""
+//                }
+//            }
+
+
+
+
+
         return safeApiCall(postData) {
             remoteDataSource.baseRequest(postData)
 
