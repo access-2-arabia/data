fce5443d5e5986e07c42241f835fb0008e4b6759
EmadShehadah <emadshehadah@Emads-MacBook-Pro.local>
2021-07-11 11:44:20 +0300

    add constructor

--- data/src/main/java/com/a2a/data/model/common/BaseResponse.kt
+++ data/src/main/java/com/a2a/data/model/common/BaseResponse.kt
@@ -2,7 +2,7 @@ package com.a2a.data.model.common
 
 import com.google.gson.annotations.SerializedName
 
-data class BaseResponse<T>(
+data class BaseResponse<T> constructor(
     @SerializedName("A2AResponse")
     var a2ARequest: A2AResponse<T>,
     @SerializedName("ErrorMsg")
