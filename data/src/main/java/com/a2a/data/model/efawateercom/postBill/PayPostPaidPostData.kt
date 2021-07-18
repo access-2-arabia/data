7005cfc5491357c1acd337e6d23e2d42d650061e
SaifJaradat <saif.jaradat1996@gmail.com>
2021-07-18 17:01:29 +0300

    Add new two api for pre and post pay.

---
+++ data/src/main/java/com/a2a/data/model/efawateercom/postBill/PayPostPaidPostData.kt
@@ -0,0 +1,74 @@
+package com.a2a.data.model.efawateercom.postBill
+
+import com.a2a.data.model.CustProfileModel
+import com.google.gson.annotations.SerializedName
+
+data class PayPostPaidPostData(
+    @SerializedName("Accounts")
+    var accounts: Accounts = Accounts(),
+    @SerializedName("Bill")
+    var bill: Bill = Bill(),
+    @SerializedName("CustProfile")
+    var custProfile: CustProfileModel = CustProfileModel()
+) {
+    data class Accounts(
+        @SerializedName("AccountFrom")
+        var accountFrom: String = "",
+        @SerializedName("AccountNumberFrom")
+        var accountNumberFrom: String = "",
+        @SerializedName("FeesAmount")
+        var feesAmount: String = "",
+        @SerializedName("PaidAmount")
+        var paidAmount: String = "",
+        @SerializedName("PaidAmt")
+        var paidAmt: String = ""
+    )
+
+    data class Bill(
+        @SerializedName("Address")
+        var address: String = "",
+        @SerializedName("BillerCode")
+        var billerCode: String = "",
+        @SerializedName("BillingNo")
+        var billingNo: String = "",
+        @SerializedName("BillingNumber")
+        var billingNumber: String = "",
+        @SerializedName("CID")
+        var cID: Int = 0,
+        @SerializedName("CustID")
+        var custID: String = "",
+        @SerializedName("DueAmount")
+        var dueAmount: Double = 0.0,
+        @SerializedName("DueAmt")
+        var dueAmt: Double = 0.0,
+        @SerializedName("eMail")
+        var eMail: String = "",
+        @SerializedName("Fees")
+        var fees: Double = 0.0,
+        @SerializedName("FeesAmount")
+        var feesAmount: Double = 0.0,
+        @SerializedName("ID")
+        var iD: String = "",
+        @SerializedName("IncPayments")
+        var incPayments: String = "",
+        @SerializedName("MobileNo")
+        var mobileNo: String = "",
+        @SerializedName("Name")
+        var name: String = "",
+        @SerializedName("PaidAmount")
+        var paidAmount: Double = 0.0,
+        @SerializedName("PaidAmt")
+        var paidAmt: Double = 0.0,
+        @SerializedName("Phone")
+        var phone: String = "",
+        @SerializedName("RepID")
+        var repID: Int = 0,
+        @SerializedName("RequestType")
+        var requestType: String = "",
+        @SerializedName("ServiceType")
+        var serviceType: String = "",
+        @SerializedName("BillerEDesc")
+        var billerEDesc: String = ""
+
+    )
+}
