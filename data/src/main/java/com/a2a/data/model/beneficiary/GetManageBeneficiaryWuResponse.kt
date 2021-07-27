b17e1c0efada7a8ba6ad41f79e15b97d9786a775
firassalaman <fsalman@access2arabia.com>
2021-07-27 10:06:14 +0300

    Build GetManageBeneficiaryWuResponse.kt

---
+++ data/src/main/java/com/a2a/data/model/beneficiary/GetManageBeneficiaryWuResponse.kt
@@ -0,0 +1,149 @@
+package com.a2a.data.model.beneficiary
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+
+data class GetManageBeneficiaryWuResponse(
+    @SerializedName("A2ARequest")
+    var a2ARequest: A2ARequest = A2ARequest(),
+    @SerializedName("A2AResponse")
+    var a2AResponse: A2AResponse = A2AResponse(),
+    @SerializedName("ErrorMsg")
+    var errorMsg: ErrorMsg = ErrorMsg()
+) {
+    data class A2ARequest(
+        @SerializedName("Body")
+        var body: Body = Body(),
+        @SerializedName("Footer")
+        var footer: Footer = Footer(),
+        @SerializedName("Header")
+        var header: Header = Header()
+    ) {
+        class Body(
+        )
+
+        data class Footer(
+            @SerializedName("Signature")
+            var signature: String = ""
+        )
+
+        data class Header(
+            @SerializedName("BankCode")
+            var bankCode: String = "",
+            @SerializedName("Channel")
+            var channel: String = "",
+            @SerializedName("ConnectorID")
+            var connectorID: String = "",
+            @SerializedName("Device")
+            var device: String = "",
+            @SerializedName("DeviceID")
+            var deviceID: String = "",
+            @SerializedName("DeviceToken")
+            var deviceToken: String = "",
+            @SerializedName("GuidID")
+            var guidID: String = "",
+            @SerializedName("MethodName")
+            var methodName: String = "",
+            @SerializedName("Password")
+            var password: String = "",
+            @SerializedName("RegionCode")
+            var regionCode: String = "",
+            @SerializedName("ServiceID")
+            var serviceID: Int = 0,
+            @SerializedName("SessionID")
+            var sessionID: String = "",
+            @SerializedName("SrvID")
+            var srvID: String = "",
+            @SerializedName("TimeStamp")
+            var timeStamp: String = "",
+            @SerializedName("UserID")
+            var userID: String = ""
+        )
+    }
+
+    data class A2AResponse(
+        @SerializedName("Body")
+        var body: Body = Body(),
+        @SerializedName("Footer")
+        var footer: Footer = Footer(),
+        @SerializedName("Header")
+        var header: Header = Header()
+    ) {
+        @Parcelize
+        data class Body(
+            @SerializedName("BeneficiaryWU")
+            var beneficiaryWU: List<BeneficiaryWU> = listOf(),
+            @SerializedName("ServiceConfiguration")
+            var serviceConfiguration: ServiceConfiguration = ServiceConfiguration()
+        ):Parcelable {
+            @Parcelize
+            data class BeneficiaryWU(
+                @SerializedName("Country")
+                var country: String = "",
+                @SerializedName("CustID")
+                var custID: String = "",
+                @SerializedName("Email")
+                var email: String = "",
+                @SerializedName("FName")
+                var fName: String = "",
+                @SerializedName("id")
+                var id: Int = 0,
+                @SerializedName("LName")
+                var lName: String = "",
+                @SerializedName("MName")
+                var mName: String = "",
+                @SerializedName("Nationality")
+                var nationality: String = "",
+                @SerializedName("PhoneNumber")
+                var phoneNumber: String = "",
+                @SerializedName("RegionCode")
+                var regionCode: String = ""
+            ):Parcelable
+
+            @Parcelize
+            data class ServiceConfiguration(
+                @SerializedName("PWDFlag")
+                var pWDFlag: Int = 0
+            ):Parcelable
+        }
+
+        data class Footer(
+            @SerializedName("Signature")
+            var signature: String = ""
+        )
+
+        data class Header(
+            @SerializedName("GuidID")
+            var guidID: String = "",
+            @SerializedName("RegionCode")
+            var regionCode: String = "",
+            @SerializedName("Result")
+            var result: Result = Result(),
+            @SerializedName("SessionID")
+            var sessionID: String = "",
+            @SerializedName("SrvID")
+            var srvID: Int = 0,
+            @SerializedName("TimeStamp")
+            var timeStamp: String = ""
+        ) {
+            data class Result(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("ErrorCode")
+                var errorCode: Int = 0
+            )
+        }
+    }
+
+    data class ErrorMsg(
+        @SerializedName("ADesc")
+        var aDesc: String = "",
+        @SerializedName("EDesc")
+        var eDesc: String = "",
+        @SerializedName("ErrorCode")
+        var errorCode: Int = 0
+    )
+}
