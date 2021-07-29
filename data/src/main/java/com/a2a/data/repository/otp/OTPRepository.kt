b936bcb67733aabc0b61d4af950608e66fef94ed
firassalaman <fsalman@access2arabia.com>
2021-07-29 11:44:19 +0300

    Build Wu Benef

--- data/src/main/java/com/a2a/data/repository/otp/OTPRepository.kt
+++ data/src/main/java/com/a2a/data/repository/otp/OTPRepository.kt
@@ -1,11 +1,57 @@
 package com.a2a.data.repository.otp
 
+import com.a2a.data.datasource.RemoteDataSource
+import com.a2a.data.model.common.A2ARequest
+import com.a2a.data.model.common.BaseRequestModel
 import com.a2a.data.repository.BaseRepository
+import com.a2a.network.Resource
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import javax.inject.Inject
 
-class OTPRepository @Inject constructor() :
+class OTPRepository @Inject constructor(private val remoteDataSource: RemoteDataSource) :
+
     BaseRepository() {
 
+    suspend fun <REQUEST, OLD_REQUEST> checkOtp(
+        request: OLD_REQUEST, utr: String?, pin: String?, isBiometric: Boolean, isManual: Boolean
+    ): Resource<REQUEST> {
+        val gson = Gson()
+        val jsonRequest = gson.toJson(request)
+        val jsonObject: JsonObject = gson.fromJson(jsonRequest, JsonObject::class.java)
+        val authJsonObject = JsonObject()
+        authJsonObject.addProperty("UTR", utr)
+        authJsonObject.addProperty("PIN", pin)
+        authJsonObject.addProperty("LWTD", isBiometric)
+        jsonObject.getAsJsonObject("A2ARequest").getAsJsonObject("Body")
+            .add("Authenticate", authJsonObject)
+        if (isManual) {
+            jsonObject.getAsJsonObject("A2ARequest").getAsJsonObject("Body")
+                .addProperty("StepNumber", "2")
 
+        }
+        return safeApiCall(jsonObject) { remoteDataSource.baseRequest(jsonObject) }
+    }
+
+    suspend fun <T> requestOTP(
+        mobileNumber: String
+    ): Resource<T> {
+
+        val postData = MemoryCacheImpl.getCustProfile()!!
+        postData.mobileNumber = mobileNumber
+
+
+        val request =
+            BaseRequestModel(
+                A2ARequest(
+                    postData,
+                    srvID = "OTPToken",
+                    serviceIDValue = 0
+                )
+            )
+        return safeApiCall(request) {
+            remoteDataSource.baseRequest(request)
+        }
+    }
 
 }
