190168dc340bff82501c7ed40369c968945166ea
khaled <khabdellatif@Access2Arabia.com>
2021-08-02 18:17:00 +0300

    remove encrypt DataHelper from Shared Preference

--- data/src/main/java/com/a2a/data/datasource/SharedPreference.kt
+++ data/src/main/java/com/a2a/data/datasource/SharedPreference.kt
@@ -3,41 +3,32 @@ package com.a2a.data.datasource
 import android.content.Context
 import android.content.SharedPreferences
 import com.a2a.network.securityHelper.EncryptionDecryptionHelper
-import javax.inject.Inject
 
 
-class SharedPreference (application: Context) {
-    private val PREFS_NAME = "BEA"
+class SharedPreference(application: Context, prefName : String) {
     private val BIOMATRIC_LOGIN = "BIOMATRIC_LOGIN"
     private val CUSTOMER_ID = "CUSTOMER_ID"
     private val PASSWORD = "PU"
     private val IS_REMEMBER_ME_ENABLED = "IS_REMEMBER_ME_ENABLED"
-    private val sharedPref: SharedPreferences =
-        application.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-    val encryptDataHelper = EncryptionDecryptionHelper()
+    private val sharedPref = application.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
     var isBiometricLoginEnabled: Boolean
         get() = sharedPref.getBoolean(BIOMATRIC_LOGIN, false)
         set(isBiometricLoginEnabled) = sharedPref.edit()
             .putBoolean(BIOMATRIC_LOGIN, isBiometricLoginEnabled).apply()
 
     var customerId: String
-        get() = encryptDataHelper.decryptData(
-            sharedPref.getString(CUSTOMER_ID, "").toString(), AppCache.token
-        )
-        set(customerId) = sharedPref.edit().putString(
-            CUSTOMER_ID, encryptDataHelper.encryptData(customerId, AppCache.token)
-        ).apply()
+        get() = sharedPref.getString(CUSTOMER_ID, "").toString()
+        set(customerId) = sharedPref.edit().putString(CUSTOMER_ID, customerId).apply()
 
     var password: String
-        get() = encryptDataHelper.decryptData(
-            sharedPref.getString(PASSWORD, "").toString(), AppCache.token
-        )
-        set(password) = sharedPref.edit().putString(
-            PASSWORD, encryptDataHelper.encryptData(password, AppCache.token)
-        ).apply()
+        get() = sharedPref.getString(PASSWORD, "").toString()
+        set(password) = sharedPref.edit().putString(PASSWORD, password).apply()
+
     var isRememberMeEnabled: Boolean
         get() = sharedPref.getBoolean(IS_REMEMBER_ME_ENABLED, false)
-        set(isRememberMeEnabled) = sharedPref.edit().putBoolean(IS_REMEMBER_ME_ENABLED, isRememberMeEnabled)
+        set(isRememberMeEnabled) = sharedPref.edit()
+            .putBoolean(IS_REMEMBER_ME_ENABLED, isRememberMeEnabled)
             .apply()
 
 
