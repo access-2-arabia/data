15eb04c3bc2ec8c6ead39b199d256852ca3c203f
SaifJaradat <saif.jaradat1996@gmail.com>
2021-08-04 10:05:09 +0300

    Add ConvertCurrency API...

--- data/src/main/java/com/a2a/data/repository/rates/RatesRepository.kt
+++ data/src/main/java/com/a2a/data/repository/rates/RatesRepository.kt
@@ -3,6 +3,7 @@ package com.a2a.data.repository.rates
 import com.a2a.data.datasource.RemoteDataSource
 import com.a2a.data.model.common.A2ARequest
 import com.a2a.data.model.common.BaseRequestModel
+import com.a2a.data.model.rate.ConvertRatePostData
 import com.a2a.data.model.rate.RateExchangePostData
 
 
@@ -17,6 +18,32 @@ class RatesRepository @Inject constructor(
     suspend fun <T> getCurrencyExChange(): Resource<T> {
 
         val body = RateExchangePostData()
+
+        val postData = BaseRequestModel(
+            A2ARequest(
+                body,
+                srvID = "FXRate"
+            )
+        )
+        return safeApiCall(postData) {
+            remoteDataSource.baseRequest(postData)
+        }
+    }
+
+    suspend fun <T> convertCurrency(
+        fromCurrency: String,
+        toCurrency: String,
+        amountToConvert: String
+    ): Resource<T> {
+
+        val body = ConvertRatePostData()
+
+        body.apply {
+            stepNumber = "2"
+            currency.fromCurrency = fromCurrency
+            currency.toCurrency = toCurrency
+            currency.amountFrom = amountToConvert
+        }
 
         val postData = BaseRequestModel(
             A2ARequest(
