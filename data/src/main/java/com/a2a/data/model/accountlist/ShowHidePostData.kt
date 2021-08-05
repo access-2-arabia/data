808f9dd5f990c7f1ec105840493c6ea05f3ffe6f
emadshehadah <eshehadah@access2arabia.com>
2021-08-05 08:54:43 +0300

    change custProfile

--- data/src/main/java/com/a2a/data/model/accountlist/ShowHidePostData.kt
+++ data/src/main/java/com/a2a/data/model/accountlist/ShowHidePostData.kt
@@ -1,6 +1,7 @@
 package com.a2a.data.model.accountlist
 
 
+import com.a2a.network.model.CustProfile
 import com.google.gson.annotations.SerializedName
 
 data class ShowHidePostData(
@@ -10,16 +11,4 @@ data class ShowHidePostData(
     var custProfile: CustProfile = CustProfile(),
     @SerializedName("StepNumber")
     var stepNumber: Int = 0
-) {
-
-    data class CustProfile(
-        @SerializedName("CID")
-        var cID: Int = 0,
-        @SerializedName("CustID")
-        var custID: String = "",
-        @SerializedName("RID")
-        var rID: Int = 0,
-        @SerializedName("RepID")
-        var repID: Any = Any()
-    )
-}
+)
