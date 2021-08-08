541836320b4d218d15e28863223784d637f6f5dd
SaifJaradat <saif.jaradat1996@gmail.com>
2021-08-08 22:31:34 +0300

    Add Add country API

--- data/src/main/java/com/a2a/data/repository/preferences/AuthorizedCountryRepository.kt
+++ data/src/main/java/com/a2a/data/repository/preferences/AuthorizedCountryRepository.kt
@@ -1,5 +1,6 @@
 package com.a2a.data.repository.preferences
 
+import MemoryCacheImpl
 import com.a2a.data.datasource.MemoryCache
 import com.a2a.data.datasource.RemoteDataSource
 import com.a2a.data.model.common.A2ARequest
@@ -12,6 +13,7 @@ import com.a2a.data.model.efawateercom.postBill.PostBillPostData
 import com.a2a.data.model.efawateercom.prePaidBill.PayPrepaidPostData
 import com.a2a.data.model.efawateercom.prePaidBill.PrepaidInquirePostData
 import com.a2a.data.model.forgetPasswod.ForgetPasswordPostData
+import com.a2a.data.model.preferneces.AddAuthorizedCountry
 import com.a2a.data.model.preferneces.GetAuthorisedCountryPostData
 import com.a2a.data.repository.BaseRepository
 import com.a2a.network.Resource
@@ -40,4 +42,31 @@ class AuthorizedCountryRepository @Inject constructor(
             remoteDataSource.baseRequest(postData)
         }
     }
+
+
+    suspend fun <T> addAuthorizedCountry(newCountry: String): Resource<T> {
+
+        val body = AddAuthorizedCountry()
+
+        val currentCustProfile = MemoryCacheImpl.getCustProfile()
+
+        body.apply {
+            if (currentCustProfile != null) {
+                custProfile = currentCustProfile
+            }
+            stepNumber = "2"
+            country = newCountry
+        }
+
+        val postData = BaseRequestModel(
+            A2ARequest(
+                body,
+                srvID = "autCountry"
+            )
+        )
+        return safeApiCall(postData) {
+            remoteDataSource.baseRequest(postData)
+        }
+    }
+
 }
