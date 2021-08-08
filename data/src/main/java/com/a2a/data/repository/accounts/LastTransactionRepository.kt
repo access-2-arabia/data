c50eb7cfbfa119e5efef5cb57d7e1f92865137c2
firassalaman <fsalman@access2arabia.com>
2021-08-08 09:57:58 +0300

    Fix getLastTransactionList

--- data/src/main/java/com/a2a/data/repository/accounts/LastTransactionRepository.kt
+++ data/src/main/java/com/a2a/data/repository/accounts/LastTransactionRepository.kt
@@ -4,6 +4,8 @@ import MemoryCacheImpl
 import com.a2a.data.constants.Constants
 import com.a2a.data.datasource.RemoteDataSource
 import com.a2a.data.extentions.formatToViewTimeStamp
+import com.a2a.data.model.common.A2ARequest
+import com.a2a.data.model.common.BaseRequestModel
 import com.a2a.data.model.lasttransaction.LastTransactionPostData
 import com.a2a.data.repository.BaseRepository
 import com.a2a.network.Resource
@@ -11,33 +13,36 @@ import java.util.*
 import javax.inject.Inject
 
 class LastTransactionRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
 ) : BaseRepository() {
 
     suspend fun <T> getLastTransactionList(
         accountNumber: String,
         toDate: String = "3/06/2021",
-        fromDate: String = "23/12/2020"
+        fromDate: String = "23/12/2020",
     ): Resource<T>? {
-        val postData = LastTransactionPostData()
-        postData.apply {
-            a2ARequest?.apply {
-                a2ARequest?.body?.apply {
-                    account.accountNumber = accountNumber
-                    branchCode = MemoryCacheImpl.getCustProfile()!!.branch
-                    custID = MemoryCacheImpl.getCustProfile()!!.custID
-                    dateFrom = fromDate
-                    dateTo = toDate
-                    indexTo = "10"
-                    transType = "A"
-                }
-                a2ARequest?.footer?.apply {
-                    signature = ""
-                }
-            }
+        val lastTransactionRepository = LastTransactionPostData()
+        lastTransactionRepository.apply {
+            body.account.accountNumber = accountNumber
+            body.branchCode = MemoryCacheImpl.getCustProfile()!!.branch
+            body.custID = MemoryCacheImpl.getCustProfile()!!.custID
+            body.dateFrom = fromDate
+            body.dateTo = toDate
+            body.indexTo = "10"
+            body.transType = "A"
         }
+
+        val postData =
+            BaseRequestModel(
+                A2ARequest(
+                    lastTransactionRepository,
+                    srvID = "LastNTrans",
+                    serviceIDValue = 0
+                )
+            )
         return safeApiCall(postData) {
             remoteDataSource.baseRequest(postData)
+
         }
     }
 
