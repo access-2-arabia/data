d7735ba466b5d5b12e81a0641eede7cd8a95af07
khaled <khabdellatif@Access2Arabia.com>
2021-08-11 13:18:42 +0300

    fixed issue

--- data/src/main/java/com/a2a/data/model/authentication/CustProfile.kt
+++ data/src/main/java/com/a2a/data/model/authentication/CustProfile.kt
@@ -6,6 +6,16 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CustProfile(
+    @SerializedName("placeOfBirth")
+    var placeOfBirth: String = "",
+    @SerializedName("AddressCity")
+    var AddressCity: String = "",
+    @SerializedName("AddressCountry")
+    var AddressCountry: String = "",
+    @SerializedName("DocValidDate")
+    var DocValidDate: String = "",
+    @SerializedName("detPrivateNationality")
+    var detPrivateNationality: String = "",
     @SerializedName("AName")
     var aName: String = "",
     @SerializedName("ANameShort")
@@ -16,6 +26,8 @@ data class CustProfile(
     var address2: String = "",
     @SerializedName("BirthDate")
     var birthDate: String = "",
+    @SerializedName("DateBirth")
+    var dateBirth: String = "",
     @SerializedName("Branch")
     var branch: String = "",
     @SerializedName("CID")
@@ -26,10 +38,16 @@ data class CustProfile(
     var custImage: String = "",
     @SerializedName("CustMnemonic")
     var custMnemonic: String = "",
+    @SerializedName("Password")
+    var password: String = "",
+    @SerializedName("PasswordOld")
+    var passwordOld: String = "",
     @SerializedName("CustType")
     var custType: Int = 0,
     @SerializedName("DocNo")
     var docNo: String = "",
+    @SerializedName("MobNo")
+    var mobNo: String = "",
     @SerializedName("DocNo1")
     var docNo1: String = "",
     @SerializedName("EMail")
@@ -40,8 +58,14 @@ data class CustProfile(
     var eNameShort: String = "",
     @SerializedName("ISOCode")
     var iSOCode: String = "",
+    @SerializedName("Gender")
+    var gender: Boolean = true,
     @SerializedName("ISOCode2")
     var iSOCode2: String = "",
+    @SerializedName("LWPT")
+    var lWPT: Boolean = false,
+    @SerializedName("LWTD")
+    var lWTD: Boolean = false,
     @SerializedName("Lang")
     var lang: String = "",
     @SerializedName("MobileNumber")
@@ -57,5 +81,17 @@ data class CustProfile(
     @SerializedName("RepID")
     var repID: String = "",
     @SerializedName("TermsAndCondition")
-    var termsAndCondition: String = ""
+    var termsAndCondition: Int = 0,
+    @SerializedName("LoanNumber")
+    var loanNumber: String = "",
+    @SerializedName("CarOwnerShip")
+    var carOwnerShip: Boolean = false,
+    @SerializedName("Occupation")
+    var occupation: String = "",
+    @SerializedName("MaritalStatus")
+    var maritalStatus: String = "",
+    @SerializedName("CustMnemonicOld")
+    var custMnemonicOld: String = "",
+    @SerializedName("CustMnemonicNew")
+    var custMnemonicNew: String = ""
 ) : Parcelable
