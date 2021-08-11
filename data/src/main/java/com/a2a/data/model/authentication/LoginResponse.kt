814e9080fa6e36de6a2304694932273ecf0e530e
khaled <khabdellatif@Access2Arabia.com>
2021-08-11 12:05:22 +0300

    update LoginResponse

--- data/src/main/java/com/a2a/data/model/authentication/LoginResponse.kt
+++ data/src/main/java/com/a2a/data/model/authentication/LoginResponse.kt
@@ -1,61 +1,53 @@
 package com.a2a.data.model.authentication
 
 
+import android.os.Parcelable
+import com.a2a.data.model.common.ErrorMsg
 import com.a2a.data.model.common.Footer
 import com.a2a.data.model.common.Header
+import com.a2a.data.model.common.HeaderResponse
 import com.a2a.network.model.CustProfile
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class LoginResponse(
-    @SerializedName("A2ARequest")
-    var a2ARequest: A2ARequest = A2ARequest(),
     @SerializedName("A2AResponse")
     var a2AResponse: A2AResponse = A2AResponse(),
     @SerializedName("ErrorMsg")
     var errorMsg: ErrorMsg = ErrorMsg()
-) {
-    data class A2ARequest(
-        @SerializedName("Body")
-        var body: Body = Body(),
-        @SerializedName("Footer")
-        var footer: Footer = Footer(),
-        @SerializedName("Header")
-        var header: Header = Header()
-    ) {
-        class Body
-
-        data class Footer(
-            @SerializedName("Signature")
-            var signature: String = ""
-        )
-
-    }
-
+) : Parcelable {
+    @Parcelize
     data class A2AResponse(
         @SerializedName("Body")
         var body: Body = Body(),
         @SerializedName("Footer")
         var footer: Footer = Footer(),
         @SerializedName("Header")
-        var header: Header = Header()
-    ) {
+        var header: HeaderResponse = HeaderResponse()
+    ) : Parcelable {
+        @Parcelize
         data class Body(
-            @SerializedName("AuthCountry")
-            var authCountry: List<Any> = listOf(),
-            @SerializedName("Authenticate")
-            var authenticate: Authenticate = Authenticate(),
-            @SerializedName("CustProfile")
-            var custProfile: CustProfile = CustProfile(),
-            @SerializedName("LoginDetails")
-            var loginDetails: LoginDetails = LoginDetails(),
             @SerializedName("ActivityLog")
             var activityLog: ActivityLog = ActivityLog(),
-            @SerializedName("OTP")
-            var oTP: OTP = OTP(),
+            @SerializedName("CustPreferences")
+            var custPreferences: List<CustPreference> = listOf(),
+            @SerializedName("CustProfile")
+            var custProfile: CustProfile = CustProfile(),
+            @SerializedName("DefaultPageMenu")
+            var defaultPageMenu: List<DefaultPageMenu> = listOf(),
+            @SerializedName("LoginDetails")
+            var loginDetails: LoginDetails = LoginDetails(),
+            @SerializedName("MainMenu")
+            var mainMenu: List<MainMenu> = listOf(),
+            @SerializedName("Menu")
+            var menu: List<Menu> = listOf(),
             @SerializedName("ServiceConfiguration")
             var serviceConfiguration: ServiceConfiguration = ServiceConfiguration(),
-
-            ) {
+            @SerializedName("TermsCondition")
+            var termsCondition: TermsCondition = TermsCondition()
+        ) : Parcelable {
+            @Parcelize
             data class ActivityLog(
                 @SerializedName("LastFailedTransfer")
                 var lastFailedTransfer: String = "",
@@ -73,50 +65,225 @@ data class LoginResponse(
                 var noOfSuccInternalTrx: Int = 0,
                 @SerializedName("NoOfSuccInternationalTrx")
                 var noOfSuccInternationalTrx: Int = 0
-            )
+            ) : Parcelable
 
+            @Parcelize
+            data class CustPreference(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("AValue")
+                var aValue: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("EValue")
+                var eValue: String = "",
+                @SerializedName("Enabled")
+                var enabled: Boolean = false,
+                @SerializedName("ID")
+                var iD: String = ""
+            ) : Parcelable
+
+            @Parcelize
+            data class CustProfile(
+                @SerializedName("AName")
+                var aName: String = "",
+                @SerializedName("ANameShort")
+                var aNameShort: String = "",
+                @SerializedName("Address1")
+                var address1: String = "",
+                @SerializedName("Address2")
+                var address2: String = "",
+                @SerializedName("BirthDate")
+                var birthDate: String = "",
+                @SerializedName("Branch")
+                var branch: String = "",
+                @SerializedName("CID")
+                var cID: Int = 0,
+                @SerializedName("CustID")
+                var custID: String = "",
+                @SerializedName("CustImage")
+                var custImage: String = "",
+                @SerializedName("CustMnemonic")
+                var custMnemonic: String = "",
+                @SerializedName("CustType")
+                var custType: Int = 0,
+                @SerializedName("DocNo")
+                var docNo: String = "",
+                @SerializedName("DocNo1")
+                var docNo1: String = "",
+                @SerializedName("EMail")
+                var eMail: String = "",
+                @SerializedName("EName")
+                var eName: String = "",
+                @SerializedName("ENameShort")
+                var eNameShort: String = "",
+                @SerializedName("ISOCode")
+                var iSOCode: String = "",
+                @SerializedName("ISOCode2")
+                var iSOCode2: String = "",
+                @SerializedName("Lang")
+                var lang: String = "",
+                @SerializedName("MobileNumber")
+                var mobileNumber: String = "",
+                @SerializedName("MobileNumberMasked")
+                var mobileNumberMasked: String = "",
+                @SerializedName("Nationality")
+                var nationality: String = "",
+                @SerializedName("NationalityID")
+                var nationalityID: String = "",
+                @SerializedName("RID")
+                var rID: Int = 0,
+                @SerializedName("RepID")
+                var repID: String = "",
+                @SerializedName("TermsAndCondition")
+                var termsAndCondition: String = ""
+            ) : Parcelable
+
+            @Parcelize
+            data class DefaultPageMenu(
+                @SerializedName("ATitle")
+                var aTitle: String = "",
+                @SerializedName("ETitle")
+                var eTitle: String = "",
+                @SerializedName("ID")
+                var iD: Int = 0,
+                @SerializedName("MenuFlag")
+                var menuFlag: Boolean = false
+            ) : Parcelable
+
+            @Parcelize
             data class LoginDetails(
                 @SerializedName("LastFailedLogin")
                 var lastFailedLogin: String = "",
+                @SerializedName("LastFailedTransfer")
+                var lastFailedTransfer: String = "",
                 @SerializedName("LastSuccessLogin")
                 var lastSuccessLogin: String = "",
-                @SerializedName("NoSuccLogin")
-                var noSuccLogin: Int = 0,
+                @SerializedName("LastSuccessTransfer")
+                var lastSuccessTransfer: String = "",
                 @SerializedName("PasswordExpiryDate")
                 var passwordExpiryDate: String = "",
                 @SerializedName("PasswordExpiryDays")
                 var passwordExpiryDays: Int = 0
-            )
+            ) : Parcelable
 
-            data class Authenticate(
-                @SerializedName("UTR")
-                var uTR: String = ""
-            )
+            @Parcelize
+            data class MainMenu(
+                @SerializedName("AGroupDesc")
+                var aGroupDesc: String = "",
+                @SerializedName("ATitle")
+                var aTitle: String = "",
+                @SerializedName("EGroupDesc")
+                var eGroupDesc: String = "",
+                @SerializedName("ETitle")
+                var eTitle: String = "",
+                @SerializedName("GroupADesc")
+                var groupADesc: String = "",
+                @SerializedName("GroupEDesc")
+                var groupEDesc: String = "",
+                @SerializedName("GroupID")
+                var groupID: Int = 0,
+                @SerializedName("ID")
+                var iD: Int = 0,
+                @SerializedName("Icon")
+                var icon: String = "",
+                @SerializedName("MenuFlag")
+                var menuFlag: Boolean = false,
+                @SerializedName("SubMenu")
+                var subMenu: List<SubMenu> = listOf()
+            ) : Parcelable {
+                @Parcelize
+                data class SubMenu(
+                    @SerializedName("AGroupDesc")
+                    var aGroupDesc: String = "",
+                    @SerializedName("ATitle")
+                    var aTitle: String = "",
+                    @SerializedName("EGroupDesc")
+                    var eGroupDesc: String = "",
+                    @SerializedName("ETitle")
+                    var eTitle: String = "",
+                    @SerializedName("Enabled")
+                    var enabled: Boolean = false,
+                    @SerializedName("GroupADesc")
+                    var groupADesc: String = "",
+                    @SerializedName("GroupEDesc")
+                    var groupEDesc: String = "",
+                    @SerializedName("GroupID")
+                    var groupID: Int = 0,
+                    @SerializedName("ID")
+                    var iD: Int = 0,
+                    @SerializedName("Icon")
+                    var icon: String = "",
+                    @SerializedName("MenuFlag")
+                    var menuFlag: Boolean = false,
+                    @SerializedName("SortIndex")
+                    var sortIndex: Int = 0,
+                    @SerializedName("URL")
+                    var uRL: String = ""
+                ) : Parcelable
+            }
 
+            @Parcelize
+            data class Menu(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("AValue")
+                var aValue: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("EValue")
+                var eValue: String = "",
+                @SerializedName("Enabled")
+                var enabled: Boolean = false,
+                @SerializedName("ID")
+                var iD: String = ""
+            ) : Parcelable
 
-            data class OTP(
-                @SerializedName("OTPLength")
-                var oTPLength: Int = 0,
-                @SerializedName("OTPTimer")
-                var oTPTimer: Int = 0
-            )
-
+            @Parcelize
             data class ServiceConfiguration(
+                @SerializedName("AccountType")
+                var accountType: String = "",
+                @SerializedName("Advertisement")
+                var advertisement: String = "",
+                @SerializedName("BankName")
+                var bankName: String = "",
+                @SerializedName("CardType")
+                var cardType: String = "",
+                @SerializedName("EStatementDate")
+                var eStatementDate: String = "",
+                @SerializedName("EStatementType")
+                var eStatementType: String = "",
+                @SerializedName("Fees")
+                var fees: String = "",
+                @SerializedName("ForgotType")
+                var forgotType: String = "",
+                @SerializedName("Layout")
+                var layout: String = "",
+                @SerializedName("LoginType")
+                var loginType: String = "",
                 @SerializedName("PWDFlag")
-                var pWDFlag: Int = 0
-            )
+                var pWDFlag: Int = 0,
+                @SerializedName("RegistrationType")
+                var registrationType: String = ""
+            ) : Parcelable
 
-
+            @Parcelize
+            data class TermsCondition(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("AText")
+                var aText: String = "",
+                @SerializedName("AValue")
+                var aValue: String = "",
+                @SerializedName("Channel")
+                var channel: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("EText")
+                var eText: String = "",
+                @SerializedName("EValue")
+                var eValue: String = ""
+            ) : Parcelable
         }
-
     }
-
-    data class ErrorMsg(
-        @SerializedName("ADesc")
-        var aDesc: String = "",
-        @SerializedName("EDesc")
-        var eDesc: String = "",
-        @SerializedName("ErrorCode")
-        var errorCode: Int = 0
-    )
 }
