29d92f6cb6c31a53974de37d34f1d2ecaa976e9d
firassalaman <fsalman@access2arabia.com>
2021-08-15 13:31:29 +0300

    Fix  Date  Locale

--- data/src/main/java/com/a2a/data/extentions/DataExtenstions.kt
+++ data/src/main/java/com/a2a/data/extentions/DataExtenstions.kt
@@ -4,24 +4,24 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun Date.formatToViewTimeStamp(): String {
-    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ", Locale.getDefault())
+    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ", Locale.ENGLISH)
     return sdf.format(this)
 }
 
 
 fun String.formatToServerDateDefaults(): String {
-    val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
     return sdf.format(this)
 }
 
 
 fun Date.formatToViewDateStamp(): String {
-    val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
     return sdf.format(this)
 }
 
 fun Date.formatToViewDateStampSlash(): String {
-    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
     return sdf.format(this)
 }
 
