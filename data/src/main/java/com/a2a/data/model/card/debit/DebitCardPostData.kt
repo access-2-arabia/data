31c5428da03833390dde87f4bf2f0c6167e188a8
firassalaman <fsalman@access2arabia.com>
2021-08-18 16:27:09 +0300

    fix  2

--- data/src/main/java/com/a2a/data/model/card/debit/DebitCardPostData.kt
+++ data/src/main/java/com/a2a/data/model/card/debit/DebitCardPostData.kt
@@ -1,16 +1,14 @@
 package com.a2a.data.model.card.debit
 
 import com.google.gson.annotations.SerializedName
-import android.os.Parcelable
 import com.a2a.network.model.CustProfile
-import kotlinx.android.parcel.Parcelize
+
 
 
 data class DebitCardPostData(
     @SerializedName("Body")
     var body: Body = Body()
 ) {
-    @Parcelize
     data class Body(
         @SerializedName("CustProfile")
         var custProfile: CustProfile = CustProfile()
