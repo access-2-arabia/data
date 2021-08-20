a2c08a8e3142012a8c19535c215de56ef13e43b4
firassalaman <fsalman@access2arabia.com>
2021-08-20 13:17:31 +0300

    Build2  Change  Mobile  Number  Credit

--- data/src/main/java/com/a2a/data/model/card/creditcard/changemobilenumber/ChangeMobileNumberCreditCardResponse.kt
+++ data/src/main/java/com/a2a/data/model/card/creditcard/changemobilenumber/ChangeMobileNumberCreditCardResponse.kt
@@ -38,13 +38,13 @@ data class ChangeMobileNumberCreditCardResponse(
             @SerializedName("Channel")
             var channel: String = "",
             @SerializedName("ConnectorID")
-            var connectorID: Any? = null,
+            var connectorID: String = "",
             @SerializedName("Device")
-            var device: Any? = null,
+            var device: String = "",
             @SerializedName("DeviceID")
-            var deviceID: Any? = null,
+            var deviceID: String = "",
             @SerializedName("DeviceToken")
-            var deviceToken: Any? = null,
+            var deviceToken: String = "",
             @SerializedName("GuidID")
             var guidID: String = "",
             @SerializedName("MethodName")
@@ -56,7 +56,7 @@ data class ChangeMobileNumberCreditCardResponse(
             @SerializedName("ServiceID")
             var serviceID: Int = 0,
             @SerializedName("SessionID")
-            var sessionID: Any? = null,
+            var sessionID: String = "",
             @SerializedName("SrvID")
             var srvID: String = "",
             @SerializedName("TimeStamp")
@@ -112,7 +112,7 @@ data class ChangeMobileNumberCreditCardResponse(
             @SerializedName("Result")
             var result: Result = Result(),
             @SerializedName("SessionID")
-            var sessionID: Any? = null,
+            var sessionID: String = "",
             @SerializedName("SrvID")
             var srvID: Int = 0,
             @SerializedName("TimeStamp")
