a3d339229561191c080ea28ee5b3049bad1877b6
firassalaman <fsalman@access2arabia.com>
2021-08-29 15:55:49 +0300

    Fix CardPaymentPostData

--- data/src/main/java/com/a2a/data/model/card/creditcard/cardpayment/CardPaymentPostData.kt
+++ data/src/main/java/com/a2a/data/model/card/creditcard/cardpayment/CardPaymentPostData.kt
@@ -6,42 +6,34 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class CardPaymentPostData(
-    @SerializedName("Body")
-    var body: Body = Body()
+    @SerializedName("Accounts")
+    var accounts: Accounts = Accounts(),
+    @SerializedName("BranchCode")
+    var branchCode: String = "",
+    @SerializedName("Cards")
+    var cards: Cards = Cards()
 ) : Parcelable {
     @Parcelize
-    data class Body(
-        @SerializedName("Accounts")
-        var accounts: Accounts = Accounts(),
-        @SerializedName("BranchCode")
-        var branchCode: String = "",
-        @SerializedName("Cards")
-        var cards: Cards = Cards(),
-        @SerializedName("StepNumber")
-        var StepNumber: String = ""
-    ) : Parcelable {
-        @Parcelize
-        data class Accounts(
-            @SerializedName("AccountNumberFrom")
-            var AccountNumberFrom: String = "",
-            @SerializedName("Amount")
-            var amount: String = "",
-            @SerializedName("Currency")
-            var currency: String = ""
-        ) : Parcelable
+    data class Accounts(
+        @SerializedName("AccountNumberFrom")
+        var accountNumberFrom: String = "",
+        @SerializedName("AccountNumberTo")
+        var accountNumberTo: String = "",
+        @SerializedName("Amount")
+        var amount: String = "",
+        @SerializedName("Currency")
+        var currency: String = "",
+        @SerializedName("CurrencyFrom")
+        var currencyFrom: String = "",
+        @SerializedName("CurrencyTo")
+        var currencyTo: String = ""
+    ) : Parcelable
 
-        @Parcelize
-        data class Cards(
-            @SerializedName("AccountNumber")
-            var accountNumber: String = "",
-            @SerializedName("Amount")
-            var amount: String = "",
-            @SerializedName("CardNumber")
-            var cardNumber: String = "",
-            @SerializedName("CurrencyFrom")
-            var currencyFrom: String = "",
-            @SerializedName("CurrencyTo")
-            var currencyTo: String = ""
-        ) : Parcelable
-    }
+    @Parcelize
+    data class Cards(
+        @SerializedName("Amount")
+        var amount: String = "",
+        @SerializedName("CardNumber")
+        var cardNumber: String = ""
+    ) : Parcelable
 }
