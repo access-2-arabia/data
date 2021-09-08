72d08df630e20547da5034449b76d1613b365ab4
firassalaman <fsalman@access2arabia.com>
2021-09-08 14:57:59 +0300

    Fix CrossCurrencyResponse

--- data/src/main/java/com/a2a/data/model/wu/feeinquire/CrossCurrencyResponse.kt
+++ data/src/main/java/com/a2a/data/model/wu/feeinquire/CrossCurrencyResponse.kt
@@ -23,7 +23,7 @@ data class CrossCurrencyResponse(
         var header: Header = Header()
     ) : Parcelable {
         @Parcelize
-        class Body() : Parcelable
+        class Body : Parcelable
 
         @Parcelize
         data class Footer(
@@ -77,82 +77,52 @@ data class CrossCurrencyResponse(
     ) : Parcelable {
         @Parcelize
         data class Body(
+            @SerializedName("AccountNumber")
+            var accountNumber: String = "",
             @SerializedName("AccountNumberFrom")
             var accountNumberFrom: String = "",
-            @SerializedName("AccountNumberTo")
-            var accountNumberTo: String = "",
             @SerializedName("AllAmt")
             var allAmt: Double = 0.0,
-            @SerializedName("Amount")
-            var amount: Double = 0.0,
-            @SerializedName("AmountAfter")
-            var amountAfter: Double = 0.0,
-            @SerializedName("AmountExtra")
-            var amountExtra: Double = 0.0,
             @SerializedName("AmountFrom")
             var amountFrom: Double = 0.0,
             @SerializedName("AmountTo")
             var amountTo: Double = 0.0,
-            @SerializedName("Amt")
-            var amt: Double = 0.0,
-            @SerializedName("AmtAfter")
-            var amtAfter: Double = 0.0,
-            @SerializedName("AmtExtra")
-            var amtExtra: Double = 0.0,
-            @SerializedName("AmtFrom")
-            var amtFrom: Double = 0.0,
-            @SerializedName("AmtNorm")
-            var amtNorm: Double = 0.0,
+            @SerializedName("AmtCr")
+            var amtCr: Double = 0.0,
+            @SerializedName("AmtDb")
+            var amtDb: Double = 0.0,
             @SerializedName("AmtTo")
             var amtTo: Double = 0.0,
+            @SerializedName("AmtToConvert")
+            var amtToConvert: Double = 0.0,
+            @SerializedName("BankRef")
+            var bankRef: String = "",
             @SerializedName("BillStatus")
             var billStatus: Int = 0,
             @SerializedName("BranchCode")
             var branchCode: String = "",
-            @SerializedName("CAmt")
-            var cAmt: Double = 0.0,
-            @SerializedName("CID")
-            var cID: Int = 0,
+            @SerializedName("CXR_RATE")
+            var cXRRATE: Double = 0.0,
+            @SerializedName("CalculatedAmount")
+            var calculatedAmount: Double = 0.0,
             @SerializedName("CanDate")
             var canDate: String = "",
             @SerializedName("Channel")
             var channel: String = "",
             @SerializedName("ChannelID")
             var channelID: Int = 0,
-            @SerializedName("Charge")
-            var charge: Double = 0.0,
-            @SerializedName("CompanyID")
-            var companyID: Int = 0,
-            @SerializedName("CurrencyFrom")
-            var currencyFrom: String = "",
-            @SerializedName("CurrencyTo")
-            var currencyTo: String = "",
-            @SerializedName("CurrentBal")
-            var currentBal: Double = 0.0,
-            @SerializedName("CustID")
-            var custID: String = "",
-            @SerializedName("CustType")
-            var custType: Int = 0,
+            @SerializedName("ComissionAmt")
+            var comissionAmt: String = "",
+            @SerializedName("Commission")
+            var commission: String = "",
             @SerializedName("DateFrom")
             var dateFrom: String = "",
-            @SerializedName("DatePost")
-            var datePost: String = "",
             @SerializedName("DateTo")
             var dateTo: String = "",
             @SerializedName("DateTrans")
             var dateTrans: String = "",
-            @SerializedName("DateValueFrom")
-            var dateValueFrom: String = "",
-            @SerializedName("DateValueTo")
-            var dateValueTo: String = "",
-            @SerializedName("DecimalPoints")
-            var decimalPoints: Int = 0,
-            @SerializedName("DepositeNo")
-            var depositeNo: String = "",
             @SerializedName("EndDate")
             var endDate: String = "",
-            @SerializedName("EqvAmount")
-            var eqvAmount: Double = 0.0,
             @SerializedName("Error")
             var error: String = "",
             @SerializedName("ErrorCode")
@@ -161,69 +131,42 @@ data class CrossCurrencyResponse(
             var errorEDesc: String = "",
             @SerializedName("ExchangeRate")
             var exchangeRate: Double = 0.0,
-            @SerializedName("ExpCode")
-            var expCode: Double = 0.0,
-            @SerializedName("FNDFlag")
-            var fNDFlag: Int = 0,
-            @SerializedName("Fees")
-            var fees: Double = 0.0,
-            @SerializedName("InOut")
-            var inOut: Int = 0,
-            @SerializedName("LimitFlag")
-            var limitFlag: Boolean = false,
+            @SerializedName("FromCurrency")
+            var fromCurrency: String = "",
+            @SerializedName("LocalAmtCr")
+            var localAmtCr: Double = 0.0,
             @SerializedName("LocalAmtCredit")
             var localAmtCredit: Double = 0.0,
-            @SerializedName("LocalAmtDebit")
-            var localAmtDebit: Double = 0.0,
+            @SerializedName("LocalAmtDb")
+            var localAmtDb: Double = 0.0,
+            @SerializedName("LocalAmtdebit")
+            var localAmtdebit: Double = 0.0,
+            @SerializedName("LocalChargeAmt")
+            var localChargeAmt: Double = 0.0,
             @SerializedName("LogID")
             var logID: Int = 0,
-            @SerializedName("NarativeLines")
-            var narativeLines: Int = 0,
-            @SerializedName("Narrative1")
-            var narrative1: String = "",
-            @SerializedName("Narrative2")
-            var narrative2: String = "",
-            @SerializedName("Narrative3")
-            var narrative3: String = "",
             @SerializedName("NextPaymentDate")
             var nextPaymentDate: String = "",
-            @SerializedName("PaidAmount")
-            var paidAmount: Double = 0.0,
-            @SerializedName("PaidAmt")
-            var paidAmt: Double = 0.0,
             @SerializedName("PortalID")
             var portalID: String = "",
-            @SerializedName("Position")
-            var position: Int = 0,
-            @SerializedName("PrincipleInterest")
-            var principleInterest: Double = 0.0,
-            @SerializedName("ProcessDate")
-            var processDate: String = "",
-            @SerializedName("RID")
-            var rID: Int = 0,
             @SerializedName("Rate")
             var rate: Double = 0.0,
-            @SerializedName("Reference")
-            var reference: String = "",
             @SerializedName("RegionCode")
             var regionCode: String = "",
             @SerializedName("ReqChannelCore")
             var reqChannelCore: String = "",
             @SerializedName("ResCode")
             var resCode: Int = 0,
-            @SerializedName("SrvID")
-            var srvID: Int = 0,
+            @SerializedName("Target_AMT")
+            var targetAMT: Double = 0.0,
+            @SerializedName("ToCurrency")
+            var toCurrency: String = "",
             @SerializedName("TotalCr")
             var totalCr: Double = 0.0,
             @SerializedName("TotalDr")
             var totalDr: Double = 0.0,
-            @SerializedName("TotalDue")
-            var totalDue: Double = 0.0,
-            @SerializedName("TransferDate")
-            var transferDate: String = "",
-            @SerializedName("ComissionAmt")
-            var comissionAmt: String = ""
-
+            @SerializedName("TreasuryRate")
+            var treasuryRate: Double = 0.0
         ) : Parcelable
 
         @Parcelize
