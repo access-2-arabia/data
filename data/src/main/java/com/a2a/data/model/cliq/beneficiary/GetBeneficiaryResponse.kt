646abfe72e8bf2ddc72dfabf68fed798263224f2
SaifJaradat <saif.jaradat1996@gmail.com>
2021-10-10 10:52:12 +0300

    add BeneficiaryIn Model..

--- data/src/main/java/com/a2a/data/model/cliq/beneficiary/GetBeneficiaryResponse.kt
+++ data/src/main/java/com/a2a/data/model/cliq/beneficiary/GetBeneficiaryResponse.kt
@@ -76,57 +76,6 @@ data class GetBeneficiaryResponse(
             @SerializedName("ServiceConfiguration")
             var serviceConfiguration: ServiceConfiguration = ServiceConfiguration()
         ) {
-            data class BeneficiaryIn(
-                @SerializedName("ADesc")
-                var aDesc: String = "",
-                @SerializedName("Address")
-                var address: String = "",
-                @SerializedName("AdminFlag")
-                var adminFlag: Boolean = false,
-                @SerializedName("AliasType")
-                var aliasType: String = "",
-                @SerializedName("Attr")
-                var attr: String = "",
-                @SerializedName("BankCode")
-                var bankCode: String = "",
-                @SerializedName("BankName")
-                var bankName: String = "",
-                @SerializedName("Branch")
-                var branch: String = "",
-                @SerializedName("City")
-                var city: String = "",
-                @SerializedName("ClientAccNo")
-                var clientAccNo: String = "",
-                @SerializedName("ClientName")
-                var clientName: String = "",
-                @SerializedName("Country")
-                var country: String = "",
-                @SerializedName("CustID")
-                var custID: Int = 0,
-                @SerializedName("EDesc")
-                var eDesc: String = "",
-                @SerializedName("Enabled")
-                var enabled: Boolean = false,
-                @SerializedName("IBAN")
-                var iBAN: String = "",
-                @SerializedName("ID")
-                var iD: Int = 0,
-                @SerializedName("ISCliq")
-                var iSCliq: Boolean = false,
-                @SerializedName("RegionCode")
-                var regionCode: String = "",
-                @SerializedName("Swift")
-                var swift: String = "",
-                @SerializedName("TblCust")
-                var tblCust: Any = Any(),
-                @SerializedName("Trusted")
-                var trusted: Boolean = false
-            ){
-                override fun toString(): String {
-                    return clientName
-                }
-            }
-
             data class BeneficiaryOut(
                 @SerializedName("ADesc")
                 var aDesc: String = "",
