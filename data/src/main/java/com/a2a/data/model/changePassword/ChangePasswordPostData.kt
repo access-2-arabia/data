cd4a062424f4631b143844d71b8d7460747abdba
firassalaman <fsalman@access2arabia.com>
2021-10-12 14:06:24 +0300

    Fix  Change  Password

--- data/src/main/java/com/a2a/data/model/changePassword/ChangePasswordPostData.kt
+++ data/src/main/java/com/a2a/data/model/changePassword/ChangePasswordPostData.kt
@@ -1,14 +1,19 @@
 package com.a2a.data.model.changePassword
 
 
+import android.os.Parcelable
+import com.a2a.network.model.CustProfile
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class ChangePasswordPostData(
-    @SerializedName("CustID")
-    var custID: String = "",
-    @SerializedName("Password")
-    var password: String = "",
-    @SerializedName("PasswordOld")
-    var passwordOld: String = ""
-
-)
+    @SerializedName("Body")
+    var body: Body = Body()
+) : Parcelable {
+    @Parcelize
+    data class Body(
+        @SerializedName("CustProfile")
+        var custProfile: CustProfile = CustProfile()
+    ) : Parcelable
+}
