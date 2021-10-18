747e70429e8be1fd83c35d5396d733653fb63659
firassalaman <fsalman@access2arabia.com>
2021-10-18 14:42:26 +0300

    Build  Paypal

---
+++ data/src/main/java/com/a2a/data/model/paypal/SendMoneyPostData.kt
@@ -0,0 +1,34 @@
+package com.a2a.data.model.paypal
+import com.a2a.network.model.CustProfile
+import com.google.gson.annotations.SerializedName
+
+
+
+data class SendMoneyPostData(
+    @SerializedName("Body")
+    var body: Body = Body(),
+) {
+
+    data class Body(
+        @SerializedName("Accounts")
+        var accounts: Accounts = Accounts(),
+        @SerializedName("CustProfile")
+        var custProfile: CustProfile = CustProfile(),
+        @SerializedName("StepNumber")
+        var stepNumber: Int = 0
+    ) {
+
+        data class Accounts(
+            @SerializedName("AccountNumberFrom")
+            var accountNumberFrom: String = "",
+            @SerializedName("Amount")
+            var amount: Int = 0,
+            @SerializedName("BranchCode")
+            var branchCode: String = "",
+            @SerializedName("CurrencyCodeFrom")
+            var currencyCodeFrom: String = "",
+            @SerializedName("SameAcc")
+            var sameAcc: String = ""
+        )
+    }
+}
---
+++ data/src/main/java/com/a2a/data/model/paypal/ValidationResponse.kt
@@ -0,0 +1,123 @@
+package com.a2a.data.model.paypal
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class ValidationResponse(
+    @SerializedName("A2ARequest")
+    var a2ARequest: A2ARequest = A2ARequest(),
+    @SerializedName("A2AResponse")
+    var a2AResponse: A2AResponse = A2AResponse(),
+    @SerializedName("ErrorMsg")
+    var errorMsg: ErrorMsg = ErrorMsg()
+) : Parcelable {
+    @Parcelize
+    data class A2ARequest(
+        @SerializedName("Body")
+        var body: Body = Body(),
+        @SerializedName("Footer")
+        var footer: Footer = Footer(),
+        @SerializedName("Header")
+        var header: Header = Header()
+    ) : Parcelable {
+        @Parcelize
+        class Body : Parcelable
+
+        @Parcelize
+        data class Footer(
+            @SerializedName("Signature")
+            var signature: String = ""
+        ) : Parcelable
+
+        @Parcelize
+        data class Header(
+            @SerializedName("BankCode")
+            var bankCode: String = "",
+            @SerializedName("Channel")
+            var channel: String = "",
+            @SerializedName("ConnectorID")
+            var connectorID: String = "",
+            @SerializedName("Device")
+            var device: String = "",
+            @SerializedName("DeviceID")
+            var deviceID: String = "",
+            @SerializedName("DeviceToken")
+            var deviceToken: String = "",
+            @SerializedName("GuidID")
+            var guidID: String = "",
+            @SerializedName("MethodName")
+            var methodName: String = "",
+            @SerializedName("Password")
+            var password: String = "",
+            @SerializedName("RegionCode")
+            var regionCode: String = "",
+            @SerializedName("ServiceID")
+            var serviceID: Int = 0,
+            @SerializedName("SessionID")
+            var sessionID: String = "",
+            @SerializedName("SrvID")
+            var srvID: String = "",
+            @SerializedName("TimeStamp")
+            var timeStamp: String = "",
+            @SerializedName("UserID")
+            var userID: String = ""
+        ) : Parcelable
+    }
+
+    @Parcelize
+    data class A2AResponse(
+        @SerializedName("Body")
+        var body: Body = Body(),
+        @SerializedName("Footer")
+        var footer: Footer = Footer(),
+        @SerializedName("Header")
+        var header: Header = Header()
+    ) : Parcelable {
+        @Parcelize
+        class Body : Parcelable
+
+        @Parcelize
+        data class Footer(
+            @SerializedName("Signature")
+            var signature: String = ""
+        ) : Parcelable
+
+        @Parcelize
+        data class Header(
+            @SerializedName("GuidID")
+            var guidID: String = "",
+            @SerializedName("RegionCode")
+            var regionCode: String = "",
+            @SerializedName("Result")
+            var result: Result = Result(),
+            @SerializedName("SessionID")
+            var sessionID: String = "",
+            @SerializedName("SrvID")
+            var srvID: Int = 0,
+            @SerializedName("TimeStamp")
+            var timeStamp: String = ""
+        ) : Parcelable {
+            @Parcelize
+            data class Result(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("ErrorCode")
+                var errorCode: Int = 0
+            ) : Parcelable
+        }
+    }
+
+    @Parcelize
+    data class ErrorMsg(
+        @SerializedName("ADesc")
+        var aDesc: String = "",
+        @SerializedName("EDesc")
+        var eDesc: String = "",
+        @SerializedName("ErrorCode")
+        var errorCode: Int = 0
+    ) : Parcelable
+}
