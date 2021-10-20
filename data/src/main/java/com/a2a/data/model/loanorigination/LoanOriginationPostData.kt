9249156d3abf448e87ff0fe7972c56b9e974097e
firassalaman <fsalman@access2arabia.com>
2021-10-20 14:41:00 +0300

    Build  LoanOrigination

---
+++ data/src/main/java/com/a2a/data/model/loanorigination/LoanOriginationPostData.kt
@@ -0,0 +1,21 @@
+package com.a2a.data.model.loanorigination
+
+import android.os.Parcelable
+import com.a2a.network.model.CustProfile
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class LoanOriginationPostData(
+    @SerializedName("Body")
+    var body: Body = Body()
+) : Parcelable {
+    @Parcelize
+    data class Body(
+        @SerializedName("CustProfile")
+        var custProfile: CustProfile = CustProfile(),
+        @SerializedName("StepNumber")
+        var stepNumber: Int = 0
+    ) : Parcelable
+}
