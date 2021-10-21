bd69b3013282bcec76a3ff7ad462a5aac03620b8
firassalaman <fsalman@access2arabia.com>
2021-10-21 14:30:55 +0300

    Fix   ValidationResponse

--- data/src/main/java/com/a2a/data/model/paypal/ValidationResponse.kt
+++ data/src/main/java/com/a2a/data/model/paypal/ValidationResponse.kt
@@ -3,7 +3,6 @@ import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
-
 @Parcelize
 data class ValidationResponse(
     @SerializedName("A2ARequest")
@@ -38,13 +37,13 @@ data class ValidationResponse(
             @SerializedName("Channel")
             var channel: String = "",
             @SerializedName("ConnectorID")
-            var connectorID: String = "",
+            var connectorID: Any? = null,
             @SerializedName("Device")
-            var device: String = "",
+            var device: Any? = null,
             @SerializedName("DeviceID")
-            var deviceID: String = "",
+            var deviceID: Any? = null,
             @SerializedName("DeviceToken")
-            var deviceToken: String = "",
+            var deviceToken: Any? = null,
             @SerializedName("GuidID")
             var guidID: String = "",
             @SerializedName("MethodName")
@@ -56,7 +55,7 @@ data class ValidationResponse(
             @SerializedName("ServiceID")
             var serviceID: Int = 0,
             @SerializedName("SessionID")
-            var sessionID: String = "",
+            var sessionID: Any? = null,
             @SerializedName("SrvID")
             var srvID: String = "",
             @SerializedName("TimeStamp")
@@ -76,7 +75,30 @@ data class ValidationResponse(
         var header: Header = Header()
     ) : Parcelable {
         @Parcelize
-        class Body : Parcelable
+        data class Body(
+            @SerializedName("Amt")
+            var amt: Double = 0.0,
+            @SerializedName("AmtTo")
+            var amtTo: Double = 0.0,
+            @SerializedName("Charge")
+            var charge: Double = 0.0,
+            @SerializedName("Commission")
+            var commission: String = "",
+            @SerializedName("ExchangeRate")
+            var exchangeRate: Double = 0.0,
+            @SerializedName("LocalAmtCredit")
+            var localAmtCredit: Double = 0.0,
+            @SerializedName("LocalAmtDebit")
+            var localAmtDebit: Double = 0.0,
+            @SerializedName("ServiceConfiguration")
+            var serviceConfiguration: ServiceConfiguration = ServiceConfiguration()
+        ) : Parcelable {
+            @Parcelize
+            data class ServiceConfiguration(
+                @SerializedName("PWDFlag")
+                var pWDFlag: Int = 0
+            ) : Parcelable
+        }
 
         @Parcelize
         data class Footer(
@@ -93,7 +115,7 @@ data class ValidationResponse(
             @SerializedName("Result")
             var result: Result = Result(),
             @SerializedName("SessionID")
-            var sessionID: String = "",
+            var sessionID: Any? = null,
             @SerializedName("SrvID")
             var srvID: Int = 0,
             @SerializedName("TimeStamp")
