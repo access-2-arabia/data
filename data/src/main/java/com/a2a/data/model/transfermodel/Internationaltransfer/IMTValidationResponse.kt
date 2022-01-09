d8b2ec1e5a4584af849316612d29a1d670ce4ea1
firassalaman <fsalman@access2arabia.com>
2022-01-09 12:48:42 +0200

    Build  IMT  Validation  Response

---
+++ data/src/main/java/com/a2a/data/model/transfermodel/Internationaltransfer/IMTValidationResponse.kt
@@ -0,0 +1,156 @@
+package com.a2a.data.model.transfermodel.Internationaltransfer
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class IMTValidationResponse(
+    @SerializedName("A2ARequest")
+    var a2ARequest: A2ARequest = A2ARequest(),
+    @SerializedName("A2AResponse")
+    var a2AResponse: A2AResponse = A2AResponse(),
+    @SerializedName("ErrorMsg")
+    var errorMsg: ErrorMsg = ErrorMsg()
+) : Parcelable {
+    @Parcelize
+    data class A2ARequest(
+        @SerializedName("Body")
+        var body: Body = Body(),
+        @SerializedName("Footer")
+        var footer: Footer = Footer(),
+        @SerializedName("Header")
+        var header: Header = Header()
+    ) : Parcelable {
+        @Parcelize
+        class Body : Parcelable
+
+        @Parcelize
+        data class Footer(
+            @SerializedName("Signature")
+            var signature: String = ""
+        ) : Parcelable
+
+        @Parcelize
+        data class Header(
+            @SerializedName("BankCode")
+            var bankCode: String = "",
+            @SerializedName("Channel")
+            var channel: String = "",
+            @SerializedName("ConnectorID")
+            var connectorID: String = "",
+            @SerializedName("Device")
+            var device: String = "",
+            @SerializedName("DeviceID")
+            var deviceID: String = "",
+            @SerializedName("DeviceToken")
+            var deviceToken: String = "",
+            @SerializedName("GuidID")
+            var guidID: String = "",
+            @SerializedName("MethodName")
+            var methodName: String = "",
+            @SerializedName("Password")
+            var password: String = "",
+            @SerializedName("RegionCode")
+            var regionCode: String = "",
+            @SerializedName("ServiceID")
+            var serviceID: Int = 0,
+            @SerializedName("SessionID")
+            var sessionID: String = "",
+            @SerializedName("SrvID")
+            var srvID: String = "",
+            @SerializedName("TimeStamp")
+            var timeStamp: String = "",
+            @SerializedName("UserID")
+            var userID: String = ""
+        ) : Parcelable
+    }
+
+    @Parcelize
+    data class A2AResponse(
+        @SerializedName("Body")
+        var body: Body = Body(),
+        @SerializedName("Footer")
+        var footer: Footer = Footer(),
+        @SerializedName("Header")
+        var header: Header = Header()
+    ) : Parcelable {
+        @Parcelize
+        data class Body(
+            @SerializedName("Amt")
+            var amt: Double = 0.0,
+            @SerializedName("AmtTo")
+            var amtTo: Double = 0.0,
+            @SerializedName("Charge")
+            var charge: Double = 0.0,
+            @SerializedName("Commission")
+            var commission: String = "",
+            @SerializedName("CurrComm")
+            var currComm: String = "",
+            @SerializedName("ExchangeRate")
+            var exchangeRate: Double = 0.0,
+            @SerializedName("FXComm")
+            var fXComm: String = "",
+            @SerializedName("IssueComm")
+            var issueComm: String = "",
+            @SerializedName("LocalAmtCredit")
+            var localAmtCredit: Double = 0.0,
+            @SerializedName("LocalAmtDebit")
+            var localAmtDebit: Double = 0.0,
+            @SerializedName("OurComm")
+            var ourComm: String = "",
+            @SerializedName("ServiceConfiguration")
+            var serviceConfiguration: ServiceConfiguration = ServiceConfiguration(),
+            @SerializedName("SwiftComm")
+            var swiftComm: String = ""
+        ) : Parcelable {
+            @Parcelize
+            data class ServiceConfiguration(
+                @SerializedName("PWDFlag")
+                var pWDFlag: Int = 0
+            ) : Parcelable
+        }
+
+        @Parcelize
+        data class Footer(
+            @SerializedName("Signature")
+            var signature: String = ""
+        ) : Parcelable
+
+        @Parcelize
+        data class Header(
+            @SerializedName("GuidID")
+            var guidID: String = "",
+            @SerializedName("RegionCode")
+            var regionCode: String = "",
+            @SerializedName("Result")
+            var result: Result = Result(),
+            @SerializedName("SessionID")
+            var sessionID: String = "",
+            @SerializedName("SrvID")
+            var srvID: Int = 0,
+            @SerializedName("TimeStamp")
+            var timeStamp: String = ""
+        ) : Parcelable {
+            @Parcelize
+            data class Result(
+                @SerializedName("ADesc")
+                var aDesc: String = "",
+                @SerializedName("EDesc")
+                var eDesc: String = "",
+                @SerializedName("ErrorCode")
+                var errorCode: Int = 0
+            ) : Parcelable
+        }
+    }
+
+    @Parcelize
+    data class ErrorMsg(
+        @SerializedName("ADesc")
+        var aDesc: String = "",
+        @SerializedName("EDesc")
+        var eDesc: String = "",
+        @SerializedName("ErrorCode")
+        var errorCode: Int = 0
+    ) : Parcelable
+}
