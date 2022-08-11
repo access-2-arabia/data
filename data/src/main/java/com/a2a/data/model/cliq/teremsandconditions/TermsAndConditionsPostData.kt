26ba2066a10e0eee51c80045c3198a9527a1302a
firassalaman <fsalman@access2arabia.com>
2022-08-11 14:47:24 +0300

    Build  Terms And  Conditions Cliq2

--- data/src/main/java/com/a2a/data/model/cliq/teremsandconditions/TermsAndConditionsPostData.kt
+++ data/src/main/java/com/a2a/data/model/cliq/teremsandconditions/TermsAndConditionsPostData.kt
@@ -1,13 +1,8 @@
 package com.a2a.data.model.cliq.teremsandconditions
 
-import android.os.Parcelable
 import com.a2a.network.model.CustProfile
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
-
-
-@Parcelize
 data class TermsAndConditionsPostData(
 
         @SerializedName("approve")
